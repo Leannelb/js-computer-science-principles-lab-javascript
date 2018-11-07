31b755f89f9fa0c2713f2301c6708a6f9eaa711b
Lillycode <leannelaceybyrne@outlook.com>
2018-11-07 19:50:50 +0100

    vars in js

--- Ch02/02_01/start/script.js
+++ Ch02/02_01/start/script.js
@@ -2,7 +2,10 @@
 
 // Define a variable with an integer value
 var myNumber = 5;
+// assign = takes value on R and assigns it to thing on L 
+//only 'number' type in js. ints floats etc. all handled by 'number'
 
 // Send the value of the variable to the output console
 console.log(myNumber);
+//when you call a method/function you use (), if you wish to pass a value, goes inside ()
 console.log(typeof myNumber);
