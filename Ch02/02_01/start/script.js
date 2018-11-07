c2d6106f95beaae5cf06cb26dd938eb57f50dfdd
Lillycode <leannelaceybyrne@outlook.com>
2018-11-07 19:54:16 +0100

    use typeof to see type of var

--- Ch02/02_01/start/script.js
+++ Ch02/02_01/start/script.js
@@ -9,3 +9,7 @@ var myNumber = 5;
 console.log(myNumber);
 //when you call a method/function you use (), if you wish to pass a value, goes inside ()
 console.log(typeof myNumber);
+// use console to see what code is doing. accesses console tool then acceses the log function of te console tool
+// pass in myNumber to tell it what to do
+// type of tells you what type the variable is. myNumber is a number
+// vars are dynamic can be changed on the fly
