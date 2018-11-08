ac91388ea58cdba8c53bef9d3683dc50f2e918e8
Lillycode <leannelaceybyrne@outlook.com>
2018-11-08 20:57:59 +0100

    3 using a function to define a var - must return a value

--- Ch02/02_06/start/script.js
+++ Ch02/02_06/start/script.js
@@ -3,7 +3,7 @@
 var subtotal = 15.00;
 //this is a literal var as the value has been specifically defined
 
-//using functions as variables
+//third way is using functions as variables - it NEEDs to return a value though
 
 console.log("Subtotal: " + subtotal);
 //if you use math: addition or subtraction, thats an evaluation
