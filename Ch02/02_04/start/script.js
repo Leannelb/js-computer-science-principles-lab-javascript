319a19fdbaee98340311527229090f0f7eb3a6cc
Lillycode <leannelaceybyrne@outlook.com>
2018-11-08 20:00:54 +0100

    function calls and console logs

--- Ch02/02_04/start/script.js
+++ Ch02/02_04/start/script.js
@@ -4,18 +4,32 @@
 var myScore;
 
 // Setup and change the game score
-init();
-changeScore();
+init(1000);
+//now im initalising newScore to 1000, without this we get NAN in console as the value was newver initalised
+changeScore(100);
+changeScore(50);
+changeScore(-250);
+changeScore(125);
 
 // Function to initialize the game score in the game
-function init()
+function init(newScore)
 {
-    myScore = 1000;
+    console.log("Setting the score to: " + newScore);
+    myScore = newScore;
+    displayScore();
 }
 
 // Function to change the score of the game
-function changeScore()
+function changeScore(scoreDelta)
+// Delta is a var name that can be used for the life of a function
+// Delta reffers to the amount of change - either up or down, its a common keyword for this
 {
-    myScore = myScore + 100;
-    console.log("Player score: " + myScore);
+    console.log("I am changing the score by: " + scoreDelta + " and the score before the changeScore function call is: " + myScore);
+    myScore = myScore + scoreDelta;
+    console.log('The new score is: ' + myScore + ' and the increment is still ' + scoreDelta);
+}
+
+function displayScore()
+{
+    console.log("Player Score: " + myScore)
 }
