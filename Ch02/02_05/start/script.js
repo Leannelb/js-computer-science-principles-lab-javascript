c770381b4259104825f05863a28c559db5815937
Lillycode <leannelaceybyrne@outlook.com>
2018-11-08 20:13:17 +0100

    vars defined OUTSIDE functions can be used inside functions.

--- Ch02/02_05/start/script.js
+++ Ch02/02_05/start/script.js
@@ -2,14 +2,15 @@
 
 // Create variable outside of a function
 var outsideValue = 5;
+//because this was defined outside the function it can be accessed from WITHIN the fucntion
 
 // Output variables to the console
-console.log("From outside: outsideValue = " + outsideValue);
-console.log("From outside: functionValue = " + functionValue);
+//console.log("From outside: outsideValue = " + outsideValue);
+//console.log("From outside: functionValue = " + functionValue);
 // gives error: script.js:8 Uncaught ReferenceError: functionValue is not defined
 // at script.js:8 because of the scope of the functionValue
 
-//myFunction();
+myFunction();
 
 //myOtherFunction();
 
