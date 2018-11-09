1d2f08144a505ff4f4bdc4929ffeaf7c99169b31
Lillycode <leannelaceybyrne@outlook.com>
2018-11-09 19:22:44 +0100

    capture user input

--- Ch03/03_01/start/script.js
+++ Ch03/03_01/start/script.js
@@ -1,4 +1,7 @@
 // JavaScript Document
+
+//capture user input
+
 var myVariable = 5;
 var msg = "my variable is equal to " + myVariable + " and is a " + typeof myVariable;
 console.log(myVariable);
--- Ch03/03_02/start/script.js
+++ Ch03/03_02/start/script.js
@@ -1,6 +1,17 @@
 // JavaScript Document
 var myVariable = 5;
 
+
+//console log type of the varible
+
+myVariable = myVariable.toString();
 var msg = "My variable is equal to " + myVariable + " and is a " + typeof myVariable;
 console.log(msg);
-//console log type of the varible
+
+myVariable = myVariable += 1;
+var msg = "My variable is equal to " + myVariable + " and is a " + typeof myVariable;
+console.log(msg);
+
+myVariable = Number(myVariable) +1;
+var msg = "my variable is equal to " + " and is a " + typeof myVariable;
+console.log(msg);
