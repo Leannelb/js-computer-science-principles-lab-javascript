7cab604c495b55f51fb2d1bcc07959c9397317d4
Lillycode <leannelaceybyrne@outlook.com>
2018-11-10 14:39:10 +0100

    How casting vars to other types works.

--- Ch03/03_01/start/script.js
+++ Ch03/03_01/start/script.js
@@ -2,6 +2,23 @@
 
 //capture user input
 
-var myVariable = 5;
-var msg = "my variable is equal to " + myVariable + " and is a " + typeof myVariable;
-console.log(myVariable);
+function init()
+{
+    document.getElementById('textOutput').innerHTML = "Enter your name then click the button";
+}
+
+function clickButton()
+{
+    var name = document.getElementById("textEntry").value;
+    //searches for an id of textEntry, hence why we called textarea id="textEntry"> so we can access and change values inside it
+    //.value
+    //chaning commands - powerful js tool getElementById returns an object, you can then execute something on this
+
+    document.getElementById("textOutput").innerHTML = createGreeting(name);
+}
+
+function createGreeting(newName)
+{
+    var greeting = "Hello, " + newName + "!";
+    return greeting;
+}
--- Ch03/03_02/start/script.js
+++ Ch03/03_02/start/script.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 var myVariable = 5;
-
+var msg = "My variable is equal to " + myVariable + " and is a " + typeof myVariable;
+console.log(msg);
 
 //console log type of the varible
 
@@ -13,5 +14,12 @@ var msg = "My variable is equal to " + myVariable + " and is a " + typeof myVari
 console.log(msg);
 
 myVariable = Number(myVariable) +1;
-var msg = "my variable is equal to " + " and is a " + typeof myVariable;
+var msg = "my variable is equal to " + myVariable + " and is a " + typeof myVariable;
 console.log(msg);
+
+//How casting vars to other types works.
+//output:
+// My variable is equal to 5 and is a number
+// script.js:10 My variable is equal to 5 and is a string
+// script.js:14 My variable is equal to 51 and is a string
+// script.js:18 my variable is equal to 52 and is a number
