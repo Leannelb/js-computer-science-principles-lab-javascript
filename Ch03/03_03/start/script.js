8604ff9cebbec500def90454059ae7fb1a9b5fe0
Lillycode <leannelaceybyrne@outlook.com>
2018-11-10 15:06:47 +0100

    if else true false

--- Ch03/03_03/start/script.js
+++ Ch03/03_03/start/script.js
@@ -1,1 +1,33 @@
 // JavaScript Document
+
+var knowJS = true; //no quotes must be lowercase
+console.log("The variable knowJS is equal to " + knowJS + " and has a type of " + typeof knowJS);
+// Prints: The variable knowJS is equal to true and has a type of boolean
+
+//know JS is true its a boolean
+if (knowJS)
+{
+    console.log("I know javascript");
+}
+
+var myValue = 5;
+console.log("My value is equal to 5");
+
+if (myValue == 5)
+{
+    console.log("This condition  (myValue == 5) is true");
+}
+
+if (myValue < 5)
+{
+    console.log("This condition (myValue < 5) is true");
+} else {
+    console.log("This condition is false");
+}
+
+// OUTPUT:
+// The variable knowJS is equal to true and has a type of boolean
+// script.js:10 I know javascript
+// script.js:14 My value is equal to 5
+// script.js:18 This condition  (myValue == 5) is true
+// script.js:25 This condition is false
