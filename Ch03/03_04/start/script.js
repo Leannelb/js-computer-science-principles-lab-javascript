918d4874b1496258e83c93d516b696fe4a3c0133
Lillycode <leannelaceybyrne@outlook.com>
2018-11-10 18:19:32 +0100

    do while loops

--- Ch03/03_04/start/script.js
+++ Ch03/03_04/start/script.js
@@ -1,1 +1,37 @@
 // JavaScript Document
+
+if (true || false)
+{
+    console.log("if any of these is true, this will be outputted");
+}
+
+if (true && false)
+{
+    console.log("This willl only output if both are true");
+}
+
+var myMaxLimit = 20;
+var myMinLimit = 5;
+myValue = 15;
+
+if (myValue < myMaxLimit && myValue > myMinLimit)
+{
+    console.log("The value " + myValue + " is between " + myMinLimit + " and " + myMaxLimit);
+} else {
+    console.log("The value " + myValue + " is not between " + myMinLimit + " and " + myMaxLimit);
+}
+
+var myMaxXLimit = 5;
+var myMinXLimit = 3;
+var myMaxYLimit = 3;
+var myMinYLimit = 1;
+
+var myXValue = 6;
+var myYValue = 2;
+
+if ((myXValue < myMaxXLimit && myXValue > myMinXLimit) && (myYValue < myMaxYLimit && myYValue > myMinYLimit))
+{
+    console.log("Both X and Y are betewwn the limits");
+} else {
+    console.log("Either X or Y is NOT betewwn the limits");
+}
--- Ch03/03_05/start/script.js
+++ Ch03/03_05/start/script.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+var myValue = 0;
+
+while (myValue < 100)
+{
+    if (myValue % 9 == 0 ){
+        console.log(myValue);
+    }
+
+    myValue ++;
+    // SAME AS:
+    // myValue = myValue +1;
+    // myValue += 1;
+}
+
+var myBadValue = 0;
+
+do
+{
+    console.log("This will run at least once");
+} while (myBadValue !=0);
