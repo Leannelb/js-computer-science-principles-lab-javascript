ee02b7bd81fda783c24302715552153a745244dc
Lillycode <leannelaceybyrne@outlook.com>
2018-11-10 18:51:16 +0100

    defining arrays

--- Ch03/03_07/start/script.js
+++ Ch03/03_07/start/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+var myArray = ["Doug", "mike", "leanne", "tom"];
+
+var myMixedArray = [4, "hello, true"];
+
+console.log(myArray);
+// script.js:6 (4) ["Doug", "mike", "leanne", "tom"]
+
+console.log(myMixedArray);
+// script.js:8 (2) [4, "hello, true"]
